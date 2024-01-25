c40acb30e7f2ff2b38338969a7ef9de8c9f5cbea
raNYnight <rany4ka@gmail.com>
2024-01-25 14:01:46 +0300

    feat: implement hash module

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,43 @@
-const calculateHash = async () => {
-    // Write your code here 
-};
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
-await calculateHash();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function calcHash() {
+  return new Promise((resolve, reject) => {
+    const filePath = path.join(
+      __dirname,
+      "files",
+      "fileToCalculateHashFor.txt"
+    );
+
+    const hash = createHash("sha256");
+    const input = createReadStream(filePath);
+
+    input.on("error", (error) => {
+      reject(error);
+    });
+
+    hash.once("readable", () => {
+      const data = hash.read();
+      if (data) {
+        resolve(data.toString("hex"));
+      } else {
+        reject(new Error("Hash calculation failed."));
+      }
+    });
+
+    input.pipe(hash);
+  });
+}
+
+calcHash()
+  .then((hash) => {
+    console.log("SHA256 hash:", hash);
+  })
+  .catch((error) => {
+    console.error("Error calculating hash:", error);
+  });
